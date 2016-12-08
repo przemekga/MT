fd12390bf25222c74a589842cb2bbeddbe017467
pemol117 <pemol10@gmail.com>
2016-12-08 18:04:02 +0100

    add exercises, generate id

--- app/js/exList.controller.js
+++ app/js/exList.controller.js
@@ -11,32 +11,37 @@
 
     function ExerciseListController(ExerciseListService) {
         var vm = this;
+        vm.exerciseGroupList = ExerciseListService.exerciseGroupList;
         vm.exerciseList = ExerciseListService.exerciseList;
-        vm.newExercise = '';
+
+        vm.exerciseGroup = {
+            id: 0,
+            groupName: '',
+            exercises: []
+        };
 
         vm.exercise = {
-            id: 0,
-            groupName: vm.newExercise,
-            exercises:
-                [
-                    {exid: 1, completed: false, exerciseName: 'Legato', duration: '10:00', bpm: 120},
-                    {exid: 2, completed: false, exerciseName: 'Staccato', duration: '12:00', bpm: 60},
-                    {exid: 3, completed: false, exerciseName: 'Trill', duration: '5:00', bpm: 90},
-                    {exid: 4, completed: false, exerciseName: 'Scales', duration: '13:00', bpm: 150},
-                    {exid: 5, completed: false, exerciseName: '4 Note per String', duration: '10:00', bpm: 140},
-                    {exid: 6, completed: false, exerciseName: 'Arpeggios', duration: '24:00', bpm: 170},
-                    {exid: 7, completed: false, exerciseName: 'Tapping', duration: '10:00', bpm: 180}
-
-                ]
+            group: 0,
+            exId: 0,
+            completed: false,
+            exerciseName: '',
+            duration: '',
+            bpm: ''
         };
 
         vm.save = function () {
-            vm.exercise.groupName = vm.newExercise;
-            ExerciseListService.save( vm.exercise );
+            ExerciseListService.save(angular.copy(vm.exerciseGroup));
+            console.log(vm.exerciseGroupList);
+        };
+
+        vm.addExercise = function (index) {
+            var temp = vm.exercise;
+            temp.group = index;
+            ExerciseListService.addExercise(angular.copy(temp));
             console.log(vm.exerciseList);
         };
 
-        console.log(vm.exercise);
+        console.log(vm.exerciseGroup);
 
     }
 })();
--- app/js/exList.service.js
+++ app/js/exList.service.js
@@ -11,41 +11,64 @@
     function ExerciseListService() {
         var exerciseService = {};
 
-        exerciseService.exerciseList = [
-            {
-                id: 1,
-                groupName: "Group 1",
-                exercises:
-                    [
-                        {exid: 1, completed: false, exerciseName: 'Legato', duration: '10:00', bpm: 120},
-                        {exid: 2, completed: false, exerciseName: 'Staccato', duration: '12:00', bpm: 60},
-                        {exid: 3, completed: false, exerciseName: 'Trill', duration: '5:00', bpm: 90},
-                        {exid: 4, completed: false, exerciseName: 'Scales', duration: '13:00', bpm: 150},
-                        {exid: 5, completed: false, exerciseName: '4 Note per String', duration: '10:00', bpm: 140},
-                        {exid: 6, completed: false, exerciseName: 'Arpeggios', duration: '24:00', bpm: 170},
-                        {exid: 7, completed: false, exerciseName: 'Tapping', duration: '10:00', bpm: 180}
-
-                    ]
-            }
+        exerciseService.exerciseGroupList = [
+            // {
+            //     id: 1,
+            //     groupName: "Group 1",
+            //     exercises:
+            //     [
+            //         {exId: 1, completed: false, exerciseName: 'Legato', duration: '10:00', bpm: 120},
+            //         {exId: 2, completed: false, exerciseName: 'Staccato', duration: '12:00', bpm: 60},
+            //         {exId: 3, completed: false, exerciseName: 'Trill', duration: '5:00', bpm: 90},
+            //         {exId: 4, completed: false, exerciseName: 'Scales', duration: '13:00', bpm: 150},
+            //         {exId: 5, completed: false, exerciseName: '4 Note per String', duration: '10:00', bpm: 140},
+            //         {exId: 6, completed: false, exerciseName: 'Arpeggios', duration: '24:00', bpm: 170},
+            //         {exId: 7, completed: false, exerciseName: 'Tapping', duration: '10:00', bpm: 180}
+            //
+            //     ]
+            // }
         ];
 
         exerciseService.getNewId = function () {
+            var maxId = exerciseService.exerciseGroupList.length;
+            exerciseService.newId = maxId + 1;
+            return exerciseService.newId;
+        };
 
-            if(exerciseService.newId) {
-                exerciseService.newId++;
-                return exerciseService.newId;
-            } else {
-                var maxId = _.max(exerciseService.exerciseList, function (entry) {return entry.id;});
-                exerciseService.newId = maxId.id + 1;
-                return exerciseService.newId;
-            }
+        exerciseService.getNewExId = function (exercise) {
+            var maxId = exerciseService.exerciseGroupList[exercise.group].exercises.length;
+            exerciseService.newExId = maxId + 1;
+            return exerciseService.newExId;
         };
 
         exerciseService.save = function (entry) {
             entry.id = exerciseService.getNewId();
-            exerciseService.exerciseList.push(entry);
+            exerciseService.exerciseGroupList.push(entry);
+        };
+        
+        exerciseService.addExercise = function (exercise) {
+            exercise.exId = exerciseService.getNewExId(exercise);
+            exerciseService.exerciseGroupList[exercise.group].exercises.push(exercise);
+            // exerciseService.exerciseGroupList.exercises.push(exercise);
         };
 
         return exerciseService;
     }
 })();
+
+
+// {
+//     id: 1,
+//     groupName: "Group 1",
+//     exercises:
+//         [
+//             {exid: 1, completed: false, exerciseName: 'Legato', duration: '10:00', bpm: 120},
+//             {exid: 2, completed: false, exerciseName: 'Staccato', duration: '12:00', bpm: 60},
+//             {exid: 3, completed: false, exerciseName: 'Trill', duration: '5:00', bpm: 90},
+//             {exid: 4, completed: false, exerciseName: 'Scales', duration: '13:00', bpm: 150},
+//             {exid: 5, completed: false, exerciseName: '4 Note per String', duration: '10:00', bpm: 140},
+//             {exid: 6, completed: false, exerciseName: 'Arpeggios', duration: '24:00', bpm: 170},
+//             {exid: 7, completed: false, exerciseName: 'Tapping', duration: '10:00', bpm: 180}
+//
+//         ]
+// }
